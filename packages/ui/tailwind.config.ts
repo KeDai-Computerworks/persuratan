a2bbca669fe6f83dc9f11172394622fe5f9481c3
Dicky Darmawan <dicky93darmawan@gmail.com>
2023-09-08 14:44:22 +0800

    chore(tailwind-config): change lib/utils

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "tailwindcss";
 
 import baseConfig from "@acme/tailwind-config/";
-import { shadcnPlugin } from "@acme/tailwind-config/lib/shadcn-plugin";
+
+import { shadcnPlugin } from "./lib/shadcn-plugin";
 
 export default {
   content: [""],
