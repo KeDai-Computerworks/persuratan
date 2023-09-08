42b18ca8119a6e219a6d2af8443087c5bfe61e0c
Dicky Darmawan <dicky93darmawan@gmail.com>
2023-09-08 14:47:11 +0800

    .

--- apps/nextjs/src/app/_components/auth-showcase.tsx
+++ apps/nextjs/src/app/_components/auth-showcase.tsx
@@ -17,7 +17,7 @@ export function AuthShowcase() {
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <p className="inline-flex items-center justify-center rounded-md bg-primary text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+      <p className="bg-primary text-primary-foreground ring-offset-background hover:bg-primary/90 focus-visible:ring-ring inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
         {session && <span>Logged in as {session.user.name}</span>}
       </p>
 
